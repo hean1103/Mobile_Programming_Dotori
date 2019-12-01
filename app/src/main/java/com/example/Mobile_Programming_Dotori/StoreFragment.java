9ce11bcce1fc3d655c078fb93edcc3bb38aa86bd
KwonBoKyeung <kbg514@naver.com>
2019-12-01 17:48:45 +0900

    아이템 구매 수정

--- app/src/main/java/com/example/Mobile_Programming_Dotori/StoreFragment.java
+++ app/src/main/java/com/example/Mobile_Programming_Dotori/StoreFragment.java
@@ -1,3 +1,4 @@
+
 package com.example.Mobile_Programming_Dotori;
 import android.content.Context;
 import android.os.AsyncTask;
@@ -10,7 +11,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -36,7 +39,10 @@ public class StoreFragment extends Fragment {
     ListView listView;
     public String id;
     TextView myPoint;
-
+    Button btn_buy; //구매 버튼
+    TextView price; //캐릭터 가격
+    TextView textView; //캐릭터 이름
+    public String sPoint;
 
     //메인액티비티로부터 bundle을 사용하여 데이터를 받아옴
     public static StoreFragment newInstance(String param) {
@@ -61,7 +67,6 @@ public class StoreFragment extends Fragment {
         //사용자의 포인트 가져오기
         GetPoint task = new GetPoint();
         task.execute(id);
-
     }
 
     @Nullable
@@ -73,58 +78,146 @@ public class StoreFragment extends Fragment {
         myPoint = (TextView)v.findViewById(R.id.myPoint);
 
         //캐릭터 리스트
-        ArrayList<String> items = new ArrayList<>();
-        items.add("cat");
-        items.add("dog");
-        items.add("ducky");
-        items.add("penguin");
-        items.add("turtle");
+        ArrayList<String[]> items = new ArrayList<>();
+        items.add(new String[] {"cat","20"});
+        items.add(new String[] {"dog","35"});
+        items.add(new String[] {"ducky","50"});
+        items.add(new String[] {"penguin","45"});
+        items.add(new String[] {"turtle","30"});
+
 
         CustomAdapter adapter = new CustomAdapter(getActivity(), 0, items);
         listView.setAdapter(adapter);
 
+
         return v;
     }
 
-    private class CustomAdapter extends ArrayAdapter<String> {
-        private ArrayList<String> items;
 
-        public CustomAdapter(Context context, int textViewResourceId, ArrayList<String> objects) {
+    private class CustomAdapter extends ArrayAdapter<String[]> {
+        private ArrayList<String[]> items;
+
+        public CustomAdapter(Context context, int textViewResourceId, ArrayList<String[]> objects) {
             super(context, textViewResourceId, objects);
             this.items = objects;
         }
 
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(final int position, View convertView, ViewGroup parent) {
             View v = convertView;
             if (v == null) {
-                LayoutInflater vi = (LayoutInflater)getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                LayoutInflater vi = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 v = vi.inflate(R.layout.store_itemlist, null);
             }
 
             // ImageView 인스턴스
-            ImageView imageView = (ImageView)v.findViewById(R.id.imageView);
+            ImageView imageView = (ImageView) v.findViewById(R.id.imageView);
+            price = (TextView) v.findViewById(R.id.price);
+            textView = (TextView)v.findViewById(R.id.textView);
 
             // 리스트뷰의 아이템에 이미지를 변경한다.
-            if("cat".equals(items.get(position)))
+            if ("cat".equals(items.get(position)[0])){
                 imageView.setImageResource(R.drawable.cat);
-            else if("dog".equals(items.get(position)))
+            }
+            else if("dog".equals(items.get(position)[0])) {
                 imageView.setImageResource(R.drawable.dog);
-            else if("ducky".equals(items.get(position)))
+            }
+            else if("ducky".equals(items.get(position)[0])) {
                 imageView.setImageResource(R.drawable.ducky);
-            else if("penguin".equals(items.get(position)))
+            }
+            else if("penguin".equals(items.get(position)[0])) {
                 imageView.setImageResource(R.drawable.penguin);
-            else if("turtle".equals(items.get(position)))
+            }
+            else if("turtle".equals(items.get(position)[0])) {
                 imageView.setImageResource(R.drawable.turtle);
+            }
+
+            textView.setText(items.get(position)[0]);
+            price.setText(items.get(position)[1]);
 
 
-            TextView textView = (TextView)v.findViewById(R.id.textView);
-            textView.setText(items.get(position));
+            btn_buy = (Button)v.findViewById(R.id.buy);
 
-            final String text = items.get(position);
+            btn_buy.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String charPrice = items.get(position)[1];
+                    if(sPoint.compareTo(charPrice) == -1) {
+                        Toast.makeText(getActivity(),"포인트가 부족합니다 ! 현재 포인트 : "+Integer.parseInt(sPoint) , Toast.LENGTH_SHORT).show();
+                    } else {
+                        int num = Integer.parseInt(sPoint) - Integer.parseInt(charPrice);
+                        BuyItem task = new BuyItem();
+                        Toast.makeText(getActivity(),"구매 후 가격 : " + num, Toast.LENGTH_SHORT).show();
+                        task.execute(Integer.toString(num), id);
+                    }
+                }
+            });
 
             return v;
         }
     }
+
+    private class BuyItem extends AsyncTask<String,Void,String> {
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+
+            try {
+                //인자로 구매하고 난 후의 가격 받아오기
+                String price = args[0];
+                String id = args[1];
+
+                String urlPath = "http://13.124.77.84/updatePoint.php?price=" + price + "&id=" + id;
+
+                URL url = new URL(urlPath);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setReadTimeout(5000);
+                conn.setConnectTimeout(5000);
+                conn.connect();
+
+                int responseStatusCode = conn.getResponseCode();
+                Log.d(TAG, "response code : " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = conn.getInputStream();
+                    Log.i("http상태 코드 : " , "HTTP_OK");
+                }
+                else{
+                    inputStream = conn.getErrorStream();
+                    Log.i("http상태 코드 : " , "NOT HTTP_OK");
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    Log.i("php 내용 가져오기 : ", line);
+                    sb.append(line);
+                }
+
+                bufferedReader.close();
+                Log.i("실행 결과 : ", sb.toString());
+                return sb.toString().trim();
+
+
+            } catch (Exception e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            myPoint.setText(result);
+            sPoint = result;
+        }
+    }
+
 
     private class GetPoint extends AsyncTask<String,Void,String> {
         protected void onPreExecute() {
@@ -170,10 +263,10 @@ public class StoreFragment extends Fragment {
                     sb.append(line);
                 }
 
-
                 bufferedReader.close();
                 Log.i("실행 결과 : ", sb.toString());
                 return sb.toString().trim();
+
 
             } catch (Exception e) {
                 return e.getMessage();
@@ -181,10 +274,10 @@ public class StoreFragment extends Fragment {
 
         }
 
-
         @Override
         protected void onPostExecute(String result){
             myPoint.setText(result);
+            sPoint = result;
         }
     }
 
