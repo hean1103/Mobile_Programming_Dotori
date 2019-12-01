810338640bf5ec5f183e9d3ee000af995edb6a62
윤진교 <jinkyo1117@naver.com>
2019-12-01 16:08:44 +0900

    Mypage upload

--- app/src/main/java/com/example/Mobile_Programming_Dotori/Login.java
+++ app/src/main/java/com/example/Mobile_Programming_Dotori/Login.java
@@ -2,6 +2,7 @@ package com.example.Mobile_Programming_Dotori;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,7 +16,7 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
-
+//로그인 구현
 public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,62 +30,74 @@ public class Login extends AppCompatActivity {
 
 
         //로그인클릭
-        LoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"로그인 성공!", Toast.LENGTH_SHORT).show();
-                String id = idText.getText().toString();
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                intent.putExtra("userid",id);
-                startActivity(intent);
-            }
-        });
-
 //        LoginButton.setOnClickListener(new View.OnClickListener() {
-//           @Override
-//           public void onClick(View v) {
-//               final String id = idText.getText().toString();
-//               final String password = pwText.getText().toString();
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(),"로그인 성공!", Toast.LENGTH_SHORT).show();
+//                String id = idText.getText().toString();
+//                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
 //
-//               Response.Listener<String> responseListener = new Response.Listener<String>() {
-//                   @Override
-//                   public void onResponse(String response) {
-//                       try{
-//                           JSONObject jsonResponse = new JSONObject(response);
-//                           boolean success = jsonResponse.getBoolean("success");
-//                           if(success)
-//                           {
-//                               String id = jsonResponse.getString("id");
-//                               String password = jsonResponse.getString("password");
+//                //id변수 공통으로 쓸수있게 하는 부분
+//                SharedPreferences pref = getSharedPreferences("pref",MODE_PRIVATE);
+//                SharedPreferences.Editor editor = pref.edit();
+//                editor.putString("Globalid",id);
+//                editor.commit();
 //
-//                               Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-//                               Toast.makeText(getApplicationContext(),"로그인 성공!", Toast.LENGTH_SHORT).show();
-//                               //로그인한 아이디 전달하기
-//                               intent.putExtra("id",id);
-//                               startActivity(intent);
-//                           }
-//                           else
-//                           {
-//                               AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
-//                               builder.setMessage("회원정보가 맞지 않습니다.")
-//                                       .setNegativeButton("다시 시도해 주세요.",null)
-//                                       .create()
-//                                       .show();
-//                           }
-//                       }catch (Exception e)
-//                       {
-//                           e.printStackTrace();
-//                       }
-//                   }
-//               };
-//               LoginRequest loginRequest = new LoginRequest(id, password, responseListener);
-//               RequestQueue queue = Volley.newRequestQueue(Login.this);
-//               queue.add(loginRequest);
-//           }
+//                startActivity(intent);
+//            }
 //        });
 
-        //회원가입 클릭
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               final String id = idText.getText().toString();
+               final String password = pwText.getText().toString();
 
+
+               //id변수 공통으로 쓸수있게 하는 부분
+               SharedPreferences pref = getSharedPreferences("pref",MODE_PRIVATE);
+               SharedPreferences.Editor editor = pref.edit();
+               editor.putString("Globalid",id);
+               editor.commit();
+
+                //php 와 통신하는 코드
+               Response.Listener<String> responseListener = new Response.Listener<String>() {
+                   @Override
+                   public void onResponse(String response) {
+                       try{
+                           JSONObject jsonResponse = new JSONObject(response);
+                           boolean success = jsonResponse.getBoolean("success");
+                           if(success)
+                           {
+                               String id = jsonResponse.getString("id");
+                               String password = jsonResponse.getString("password");
+
+                               Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                               Toast.makeText(getApplicationContext(),"로그인 성공!", Toast.LENGTH_SHORT).show();
+                               startActivity(intent);
+                           }
+                           else
+                           {
+                               AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
+                               builder.setMessage("회원정보가 맞지 않습니다.")
+                                       .setNegativeButton("다시 시도해 주세요.",null)
+                                       .create()
+                                       .show();
+                           }
+                       }catch (Exception e)
+                       {
+                           e.printStackTrace();
+                       }
+                   }
+               };
+               //Volley 라이브러리를 이용해서 실제 서버와 통신을 구현하는 부분
+               LoginRequest loginRequest = new LoginRequest(id, password, responseListener);
+               RequestQueue queue = Volley.newRequestQueue(Login.this);
+               queue.add(loginRequest);
+           }
+        });
+
+        //회원가입 클릭
         RegisterButton.setOnClickListener(new View.OnClickListener() {
                                               @Override
                                               public void onClick(View v) {
--- app/src/main/java/com/example/Mobile_Programming_Dotori/MypageFragment.java
+++ app/src/main/java/com/example/Mobile_Programming_Dotori/MypageFragment.java
@@ -1,17 +1,153 @@
 package com.example.Mobile_Programming_Dotori;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MypageFragment extends Fragment {
+    String myJSON;
 
+    private static final String TAG_RESULTS = "result";
+    private static final String TAG_ID = "id";
+    private static final String TAG_Password = "password";
+    private static final String TAG_Phone = "phone_number";
+    private static final String TAG_Birth = "birth_date";
+    private static final String TAG_Name = "name";
+    private static final String TAG_Point = "point";
+
+
+    TextView vID;
+    TextView vPhone;
+    TextView vBirth;
+    TextView vName;
+    TextView vPoint;
+    Button Logout;
+
+    ArrayList<HashMap<String,String>> personList;
+
+
+    String url = "http://13.124.77.84/Mypage.php";
+
+    JSONArray peoples = null;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_mypage, container, false);
+        View root = inflater.inflate(R.layout.fragment_mypage, container, false);
+        vPhone = (TextView) root.findViewById(R.id.my_phone);
+        vBirth = (TextView) root.findViewById(R.id.my_birth);
+        vName = (TextView) root.findViewById(R.id.my_name);
+        vPoint = (TextView) root.findViewById(R.id.my_point);
+
+        Logout = (Button) root.findViewById(R.id.btn1);
+
+        //공통으로 사용되는 id 변수 불러오기
+        SharedPreferences pref = this.getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
+        final String g_id = pref.getString("Globalid","");
+        //final String g_id = "jk";
+
+        personList = new ArrayList<HashMap<String, String>>();
+
+        class GetDataJSON extends AsyncTask<String,Void,String> {
+            @Override
+            protected String doInBackground(String... params){
+                String uri = params[0];
+
+                BufferedReader bufferedReader = null;
+                try{
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while((json=bufferedReader.readLine())!=null)
+                    {
+                        sb.append(json+"\n");
+                    }
+                    return sb.toString().trim();
+                }catch(Exception e){
+                    return null;
+                }
+            }
+
+
+            protected void showList()
+            {
+
+                try
+                {
+                    JSONObject jsonObj = new JSONObject(myJSON);
+                    peoples = jsonObj.getJSONArray(TAG_RESULTS);
+
+                    for(int i=0;i<peoples.length();i++)
+                    {
+
+                        JSONObject c = peoples.getJSONObject(i);
+                        String id = c.getString(TAG_ID);
+                        String password = c.getString(TAG_Password);
+                        String name = c.getString(TAG_Name);
+                        String phone_number = c.getString(TAG_Phone);
+                        String birth_date = c.getString(TAG_Birth);
+                        String point = c.getString(TAG_Point);
+
+                        HashMap<String,String> persons = new HashMap<String,String>();
+
+                        if(id.equals(g_id))
+                        {
+                            vPoint.setText(point);
+                            vName.setText(name);
+                            vBirth.setText(birth_date);
+                            vPhone.setText(phone_number);
+                            break;
+                        }
+
+                    }
+
+                } catch(JSONException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            protected void onPostExecute(String result)
+            {
+                myJSON = result;
+                showList();
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute(url);
+
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+
+
+
+        return root;
     }
 }
