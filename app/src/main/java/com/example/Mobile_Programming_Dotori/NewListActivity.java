7d2c1f592cb517d551436062d737a191b2b7ba3d
lee951229 <56010733+lee951229@users.noreply.github.com>
2019-12-03 15:47:27 +0900

    Update NewListActivity.java

--- app/src/main/java/com/example/Mobile_Programming_Dotori/NewListActivity.java
+++ app/src/main/java/com/example/Mobile_Programming_Dotori/NewListActivity.java
@@ -1,23 +1,38 @@
-package com.example.list;
+package com.example.Mobile_Programming_Dotori;
 
-
+import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
+import android.nfc.Tag;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.Calendar;
+import java.util.Locale;
 
 
 public class NewListActivity extends AppCompatActivity {
