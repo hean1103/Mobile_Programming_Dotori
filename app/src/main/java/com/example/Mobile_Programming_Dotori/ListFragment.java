69c59a6d269f2d5ef0f85cb97b919b5e1d79fc7e
lee951229 <56010733+lee951229@users.noreply.github.com>
2019-12-03 23:35:12 +0900

    Update ListFragment.java

--- app/src/main/java/com/example/Mobile_Programming_Dotori/ListFragment.java
+++ app/src/main/java/com/example/Mobile_Programming_Dotori/ListFragment.java
@@ -31,14 +31,15 @@ import static android.content.ContentValues.TAG;
 public class ListFragment extends Fragment {
     String data[];
     int[] checkData;
+    int listNum;
 
+    //    SharedPreferences pref = this.getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
+    //final String id = pref.getString("Globalid","");
 
+    String PName = "aaa";
+    String id = "hean";
 
-    SharedPreferences pref = this.getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
-    final String id = pref.getString("Globalid","");
-    String PName;
-
-    ListView listview ;
+    ListView listview;
     listAdapter adapter;
 
     @Nullable
@@ -50,35 +51,35 @@ public class ListFragment extends Fragment {
         listview.setAdapter(adapter);
 
         get_data task = new get_data(); // 프로젝트 리스트들을 얻기 위한 객체
-        task.execute(id,PName); //@@@@@@@@@@
-        FloatingActionButton fab = view.findViewById(R.id.fab_sub1); // 새로운 프로젝트 추가를 위한 + 버튼
+        task.execute(id, PName); //@@@@@@@@@@
+        FloatingActionButton fab = view.findViewById(R.id.fab_sub1); // 새로운 리스트 추가를 위한 + 버튼
         fab.setOnClickListener(new FABClickListener());
 
-        return view ;
-
+        return view;
     }
 
-    class FABClickListener implements  View.OnClickListener{
-            @Override
+    class FABClickListener implements View.OnClickListener {
+        @Override
         // + 버튼을 클릭시 NewListActivity.java로 화면 전환
         public void onClick(View v) {
-            Intent intent = new Intent(getActivity(),NewListActivity.class); //회원아이디
+            Intent intent = new Intent(getActivity(), NewListActivity.class); //회원아이디
             intent.putExtra("pid", id); // 아이디 넘기기 //@@@@@@@@@@@
-            intent.putExtra("pName", PName); // 아이디 넘기기 //@@@@@@@@@@@
+            intent.putExtra("pName", PName); // 프로젝트 이름 넘기기 //@@@@@@@@@@@
             startActivity(intent);
         }
     }
+
     //리스트들을 얻기 위한 함수
     public class get_data extends AsyncTask<String, Void, String> {  // 비동기 클래스
         protected void onPreExecute() { // 실행전 수행되는 함수
         }
 
         @Override
-        protected String doInBackground (String...params){
+        protected String doInBackground(String... params) {
             try {
                 String id = params[0];
                 String PName = params[1];
-                String uri = "http://13.124.77.84/getprojectlist.php?PID="+id + "&PName=" + PName; // 회원 아이디를 변수로 php에 넘겨줌 디비 php 경로
+                String uri = "http://13.124.77.84/getprojectlist.php?PID=" + id + "&PName=" + PName; // 회원 아이디와 프로젝트이름을 변수로 php에 넘겨줌 디비 php 경로
                 URL url = new URL(uri);
                 // httpURLConnection을 통해 data를 가져온다.
                 HttpURLConnection httpsURLConnection = (HttpURLConnection) url.openConnection();
@@ -92,13 +93,12 @@ public class ListFragment extends Fragment {
 
                 InputStream inputStream;
                 // php문으로부터 온 응답코드가 200일 경우
-                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                if (responseStatusCode == HttpURLConnection.HTTP_OK) {
                     inputStream = httpsURLConnection.getInputStream();
-                    Log.i("http상태 코드 : " , "HTTP_OK");
-                }
-                else{
+                    Log.i("http상태 코드 : ", "HTTP_OK");
+                } else {
                     inputStream = httpsURLConnection.getErrorStream();
-                    Log.i("http상태 코드 : " , "NOT HTTP_OK");
+                    Log.i("http상태 코드 : ", "NOT HTTP_OK");
                 }
                 // 요청 결과물 받기
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
@@ -108,7 +108,7 @@ public class ListFragment extends Fragment {
                 String line;
 
                 //php문 결과 내용 가져오기 라인으로 받아오기
-                while((line = bufferedReader.readLine()) != null){
+                while ((line = bufferedReader.readLine()) != null) {
                     sb.append(line);
                 }
 
@@ -121,14 +121,14 @@ public class ListFragment extends Fragment {
             } catch (MalformedURLException e) {
                 return e.getMessage();
             } catch (IOException e) {
-                Log.e("log_tag1", "Error converting result "+e.toString());
+                Log.e("log_tag1", "Error converting result " + e.toString());
                 return e.getMessage();
             }
         }
 
 
         @Override
-        protected void onPostExecute(String result){
+        protected void onPostExecute(String result) {
             // 프로젝트 리스트들을 listview 형태로 만들기 위한 선언
             adapter = new listAdapter();
             listview.setAdapter(adapter);
@@ -138,19 +138,24 @@ public class ListFragment extends Fragment {
                 JSONArray jsonArray = new JSONArray(result);
                 JSONObject jsonObject = null;
                 data = new String[jsonArray.length()]; // 배열의 길이만큼 크기 선언
+                checkData = new int[jsonArray.length()];
 
                 for (int i = 0; i < jsonArray.length(); i++) {
-                    // 배열을 하나씩 객체로 받아 그 안의 ListName이라고 저장되있는 값을 data배열에 넣어줌 == 프로젝트 이름
+                    // 배열을 하나씩 객체로 받아 그 안의 ListName이라고 저장되있는 값을 data배열에 넣어줌 == 리스트 이름
                     jsonObject = jsonArray.getJSONObject(i);
-                    data[i] = jsonObject.getString("ListName"); // column name
+                    data[i] = jsonObject.getString("ListName"); // 리스트 이름
                     checkData[i] = jsonObject.getInt("CheckBox"); //@@@@@@@@@@@@@@
                     Log.i("php 내용 가져오기 : ", data[i]);
                 }
 
-                for(int k = 0 ; k < data.length ; k++) {
+                for (int k = 0; k < data.length; k++) {
                     //data 배열의 크기만큼 for문을 돌며 커스텀 listview 생성
-                    adapter.addItem( ContextCompat.getDrawable( getActivity(), R.drawable.menu ),
-                            data[k], "Account Circle Black 36dp", checkData[k] ) ; //@@@@@@@@@@@@@@@@@@@@
+                    adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.menu),
+                            data[k], "Account Circle Black 36dp", checkData[k]);
+
+                    if(checkData[k]==1){
+                        listNum++;
+                    }
 
                 }
             } catch (Exception e) {
