305bfabea2dff7f8391afcf0443229d942aefee4
KwonBoKyeung <kbg514@naver.com>
2019-12-05 05:11:18 +0900

    php file upload

--- PHP_File/listdelete.php
+++ PHP_File/listdelete.php
@@ -6,19 +6,16 @@ if (mysqli_connect_errno($con))
 }
 $PID = $_GET['PID'];
 $PName = $_GET['PName'];
-$NewListName = $_GET['NewListName'];
-$Memo = $_GET['Memo'];
 $ListName = $_GET['ListName'];
-//$query = "Select ListName from list";
 
-$query = "UPDATE list SET ListName='$NewListName', Memo='$Memo' WHERE PID='$PID' AND PName='$PName' AND ListName='$ListName'";
+$query = "DELETE FROM list WHERE PID='$PID' AND PName='$PName' AND ListName='$ListName'";
 
 $result = mysqli_query($con,$query);
 if($result){
-    echo $NewListName;
-  }
-  else{
-    echo mysql_error();
-  }
+        echo $ListName;
+}
+else{
+        echo mysql_error();
+}
 mysqli_close($con);
 ?>
