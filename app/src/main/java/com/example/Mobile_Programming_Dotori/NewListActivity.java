8e9527356a133125925745e2e54ef4fbaeee692c
KwonBoKyeung <kbg514@naver.com>
2019-12-05 02:21:31 +0900

    리스트 추가 기능 구현

--- app/src/main/java/com/example/Mobile_Programming_Dotori/NewListActivity.java
+++ app/src/main/java/com/example/Mobile_Programming_Dotori/NewListActivity.java
@@ -37,42 +37,49 @@ import java.util.Locale;
 
 public class NewListActivity extends AppCompatActivity {
 
-//    String tmp_name;
-//    String tmp_listname;
-//    String tmp_memo;
-//
-//    EditText listname;
-//    EditText memo;
-//
-//    public String id;
+    String tmp_name;
+    String tmp_memo;
+
+    EditText listname;
+    EditText memo;
+
+    public String PID;
+    public String PName;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newlist); // layout 연결
-//        //EditText에 입력된 값들을 가져옴
-//        listname = (EditText) findViewById(R.id.listname);
-//        memo  = (EditText) findViewById(R.id.memo);
-//
-//        //Add 버튼을 눌렀을 경우
-//        Button add = (Button)findViewById(R.id.listAdd);
-//        add.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // 저장된 정보들을 가져옴
-//                tmp_name = listname.getText().toString();
-//                tmp_memo = memo.getText().toString();
-//
-//                if (tmp_name.length() == 0) { // 리스트의 이름이 입력되지 않았을 경우
-//                    Toast.makeText(getApplicationContext(), "리스트의 이름을 입력해주세요.", Toast.LENGTH_SHORT).show();
-//                }
-//                else {
-//                    // 새로운 리스트 정보를 DB에 넣기 위한 기능 (매개변수로 설정)
-//                    insertoToDatabase(id,tmp_name, tmp_listname, tmp_memo);
-//                }
-//            }
-//        });
+
+        Intent intent = getIntent(); /*데이터 수신*/
+        PID = intent.getExtras().getString("pid");
+        PName = intent.getExtras().getString("pname");
+
+        //EditText에 입력된 값들을 가져옴
+        listname = (EditText) findViewById(R.id.listname);
+        memo  = (EditText) findViewById(R.id.memo);
+
+        //Add 버튼을 눌렀을 경우
+        Button add = (Button)findViewById(R.id.listAdd);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 저장된 정보들을 가져옴
+                tmp_name = listname.getText().toString();
+                tmp_memo = memo.getText().toString();
+
+                if (tmp_name.length() == 0) { // 리스트의 이름이 입력되지 않았을 경우
+                    Toast.makeText(getApplicationContext(), "리스트의 이름을 입력해주세요.", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    // 새로운 리스트 정보를 DB에 넣기 위한 기능 (매개변수로 설정)
+                    insertoToDatabase(PID, PName, tmp_name, tmp_memo);
+                    finish();
+                }
+            }
+        });
+
     }
     //서버의 PHP문을 사용하여 DB에 데이터를 삽입하는 함수
     private void insertoToDatabase(String PID, String PName, String ListName, String Memo) {
@@ -97,23 +104,20 @@ public class NewListActivity extends AppCompatActivity {
                 String ListName = (String) params[2];
                 String Memo = (String) params[3];
                 // 회원 아이디와 프로젝트 이름, 리스트 이름, 메모를 변수로 넘겨줌
-                String data = "PID="+ PID +"&PName=" + PName + "&ListName=" + ListName +"&Memo=" + Memo;
+
                 try {
                     //서버의 php문에 연결
-                    URL url = new URL("http://13.124.77.84/getList.php");
+                    String urlpath = "http://13.124.77.84/listupdate.php?PID="+PID +"&PName=" + PName + "&ListName=" + ListName +"&Memo=" + Memo; // 회원 아이디를 변수로 php에 넘겨줌
+
+                    URL url = new URL(urlpath);
 
                     // httpURLConnection을 통해 data를 가져온다.
                     HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
 
                     httpURLConnection.setReadTimeout(5000);
                     httpURLConnection.setConnectTimeout(5000);
-                    httpURLConnection.setRequestMethod("POST"); // Post 데이터 통신을 사용
+                    httpURLConnection.setRequestMethod("GET");
                     httpURLConnection.connect();
-
-                    OutputStream outputStream = httpURLConnection.getOutputStream();
-                    outputStream.write(data.getBytes("UTF-8"));
-                    outputStream.flush(); // 버퍼링된 출력 바이트 실행
-                    outputStream.close();
 
                     // php문 응답 코드 확인하기
                     int responseStatusCode = httpURLConnection.getResponseCode();
@@ -140,10 +144,8 @@ public class NewListActivity extends AppCompatActivity {
 
 
                     bufferedReader.close();
-                    finish();
                     // onPseExecute로 결과값 전달
                     return sb.toString();
-
 
                 } catch (Exception e) {
 
--- app/src/main/java/com/example/Mobile_Programming_Dotori/SublistActivity.java
+++ app/src/main/java/com/example/Mobile_Programming_Dotori/SublistActivity.java
@@ -7,6 +7,7 @@ import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +17,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -43,6 +45,7 @@ public class SublistActivity extends AppCompatActivity {
     public String aaa;
     ListView listView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i("Log" , "여기는 sublistActivity.java에요 ㅡㅡㅡㅡㅡㅡㅡ");
@@ -56,7 +59,6 @@ public class SublistActivity extends AppCompatActivity {
         Log.i("Log" , "ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡpid :" + pid);
         Log.i("Log" , "ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡpname :" + pname);
 
-
         get_data task = new get_data();
         try {
             //String myChar에 이미지 이름을 받아옴
@@ -66,9 +68,6 @@ public class SublistActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        //캐릭터 리스트
-        ArrayList<String> items = new ArrayList<>();
-        //사용자의 캐릭터 이름들은 string이며 ,(comma)로 구분되어 있기 때문에 이를 구분하여 리스트에 담음
         String str[] = aaa.split(","); //각 리스트들이 리스트이름, 체크박스 형태로 되어있음
 
         ArrayList<String> listName = new ArrayList<>();
@@ -90,15 +89,17 @@ public class SublistActivity extends AppCompatActivity {
         listAdapter adapter = new listAdapter(getApplicationContext(), 0, listName);
         listView.setAdapter(adapter);
 
-        FloatingActionButton fab = findViewById(R.id.fab_sub1); // 새로운 리스트 추가를 위한 + 버튼
-//        fab.setOnClickListener(new FloatingActionButton.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(getApplicationContext(),NewListActivity.class);
-//                startActivity(intent); //액티비티로 화면 전환
-//            }
-//
-//        });
+
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab_sub1); // 새로운 프로젝트 추가를 위한 + 버튼
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),NewListActivity.class);
+                intent.putExtra("pid",pid);
+                intent.putExtra("pname",pname);
+                startActivity(intent);
+            }
+        });
     }
 
 
@@ -119,9 +120,18 @@ public class SublistActivity extends AppCompatActivity {
 
             TextView textView = (TextView)v.findViewById(R.id.textView);
 
-            // 이미지의 이름을 넣어줌
             textView.setText(items.get(position));
 
+            CheckBox listCheck = (CheckBox)v.findViewById(R.id.listCheck);
+
+            //checkBox가 1이면 0으로, 0이면 1로 바꿈
+            listCheck.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    updateCheck task = new updateCheck();
+                    task.execute(pid, pname,items.get(position));
+                }
+            });
             return v;
         }
     }
@@ -184,4 +194,64 @@ public class SublistActivity extends AppCompatActivity {
             Log.i("tag", result);
         }
     }
+
+    public class updateCheck extends AsyncTask<String, Void, String> {  // 비동기 클래스
+        protected void onPreExecute() { // 실행전 수행되는 함수
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                String PID = params[0];
+                String PName = params[1];
+                String ListName = params[2];
+                Log.i("PName : ", PName);
+
+                String urlPath = "http://13.124.77.84/updateCheck.php?PID=" + PID + "&PName=" + PName + "&ListName=" + ListName; // 회원 아이디와 프로젝트이름을 변수로 php에 넘겨줌 디비 php 경로
+
+                URL url = new URL(urlPath);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");  //GET 방식을 사용함 (default 방식)
+                conn.setReadTimeout(5000);
+                conn.setConnectTimeout(5000);
+                conn.connect();
+
+                int responseStatusCode = conn.getResponseCode();
+                Log.d(TAG, "response code : " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = conn.getInputStream();
+                    Log.i("http상태 코드 : " , "HTTP_OK");
+                }
+                else{
+                    inputStream = conn.getErrorStream();
+                    Log.i("http상태 코드 : " , "NOT HTTP_OK");
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    Log.i("php 내용 가져오기 : ", line);
+                    sb.append(line);
+                }
+
+                bufferedReader.close();
+                Log.i("실행 결과 : ", sb.toString());
+                return sb.toString().trim();
+
+            } catch (Exception e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.i("tag", result);
+        }
+    }
+
 }
